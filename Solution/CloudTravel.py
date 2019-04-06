12f732e915d13dff0659bc1274699a73bc0e122c
ajaykmahar <ajay.mhar@gmail.com>
2019-04-06 19:02:14 +0530

    adding cloud travel solution

---
+++ Solution/CloudTravel.py
@@ -0,0 +1,67 @@
+from math import radians, cos, sin, asin, sqrt
+
+class CloudTravel:
+    #Declaring const variable 
+    RADIUS = 4000
+    MAXINT = 9223372036854775807
+    #Initializing private data
+    def __init__(self):
+        self.listOfDistances = [[__class__.MAXINT for i in range(3)] for j in range(3)] # will contain list of list distances
+
+    def distance(self, latitude1, longitude1, latitude2, longitude2):
+        #========STARTING Calculating travling distance==============
+        # convert degrees to radians for every longitude and latitude
+        # convert decimal degrees to radians 
+        longitude1, latitude1, longitude2, latitude2 = map(radians, [longitude1, latitude1, longitude2, latitude2])
+
+        #=====DEBUGGING==================
+        #======END=======================
+
+        # haversine formula 
+        dlon = longitude2 - longitude1 
+        dlat = latitude2 - latitude1
+        a = sin(dlat/2)**2 + cos(latitude1) * cos(latitude2) * sin(dlon/2)**2
+        c = 2 * asin(sqrt(a)) 
+        # Radius of earth in miles
+        return __class__.RADIUS * c
+        #========END OF Calculating travling distance==============
+
+    #Calculate the great circle distance between two points on the earth.
+    #    
+    def shortestTrip(self, latitude, longitude, canTravel, origin, destination):
+       
+        # Can't be destination is the same as our point of origin
+        if origin == destination:
+           return 0.0
+
+        for index in range(len(canTravel)):
+            #=====DEBUGGING==================
+            #print(int(canTravel[index].replace(" ", "")))
+            #======END=======================
+            dest = int(canTravel[index].replace(" ", "")) #getting canTravel from tuple and parsing it to int
+            if dest >= 10: # checking if it's two digit number
+                while (dest > 0):
+                    dest = dest //10 # Calculating distance digit by digit
+                    self.listOfDistances[index][dest] = self.distance(latitude[index], longitude[index], latitude[dest], longitude[dest])
+            else:
+                # Else dest is single digit then simply calculate the distance
+                self.listOfDistances[index][dest] = self.distance(latitude[index], longitude[index], latitude[dest], longitude[dest])
+        
+        # Finding minimum distance for all the available path based on traveling time
+        for i in range(len(latitude)):
+            for j in range(len(latitude)):
+                for k in range(len(latitude)):
+                    self.listOfDistances[j][k] = min(self.listOfDistances[j][k], self.listOfDistances[j][i] + self.listOfDistances[i][k])
+        
+        return self.listOfDistances[origin][destination]
+        
+        
+
+if __name__ == '__main__':
+    obj = CloudTravel()
+    print(obj.shortestTrip((0, 0, 70), (90, 0, 45), ("2", "0 2", "0 1"), 0, 1))
+    print(obj.shortestTrip((0, 0, 70), (90, 0, 45), ("1 2", "0 2", "0 1"), 0, 1))
+    print(obj.shortestTrip((0, 30, 60), (25, -130, 78), ("1 2", "0 2", "1 2"), 0, 0))
+    print(obj.shortestTrip((0, 20, 55), (-20, 85, 42), ("1", "0", "0"), 0, 2))
+    
+    
---
+++ Solution/test_CloudTravel.py
@@ -0,0 +1,79 @@
+"""
+Constraints
+ • latitude, longitude, and canTravel will contain between 1 and 20 elements, inclusive.
+ •latitude, longitude, and canTravel will each contain the same number of elements.
+ •Each element of latitude will be between -89 and 89, inclusive.
+ •Each element of longitude will be between -179 and 179, inclusive.
+ •Each element of canTravel will be a space-delimited list of integers, with no leading zeroes.
+
+•Each integer represented in each element of canTravel will be between 0 and n - 1, where n is the number of elements in latitude.
+•origin and destination will be between 0 and n - 1, inclusive, where n is the number of elements in latitude.
+•No two airports will reside at the same latitude and longitude.
+
+ Examples
+  0)
+    {0, 0, 70}
+    {90, 0, 45}
+    {"2", "0 2", "0 1"}
+    0
+    1
+    Returns: 10612.237799994255
+    Here, we are looking to travel from airport 0 to airport 1.
+    Using the given formula, we calculate that the distance from 0 to 1 is 6283, from 0 to 2 is 5306, and from 1 to 2 is 5306.
+    A direct route from airport 0 to 1 would be fastest, if such a route were allowed.
+    Since it is not, we have to travel through airport 2.
+  
+   1)
+    {0, 0, 70}
+    {90, 0, 45}
+    {"1 2", "0 2", "0 1"}
+    0
+    1
+    Returns: 6283.185307179586
+    Here, we have the same three airports, and there is a safe route between any two.
+    Thus, we take the direct route, which is quickest.
+    
+    2)
+     {0, 30, 60}
+     {25, -130, 78}
+     {"1 2", "0 2", "1 2"}
+     0 
+     0
+     Returns: 0.0
+     We are free to travel as we wish, but since our destination is the same as our point of origin,
+     we don't have much travel to do.
+     
+     3)
+     {0, 20, 55}
+     {-20, 85, 42}
+     {"1", "0", "0"}
+     0
+     2
+     Returns:
+     Notice here that we could go from airport 2 to airport 0, but not from 0 to 2.
+     Given the available routes, there is no way we can get from airport 0 to airpor
+"""
+
+import Solution.CloudTravel as cloud
+
+list_of_latitude = [(0, 0, 70), (0, 0, 70), (0, 30, 60), (0, 20, 55),]
+lsit_of_longitude = [(90, 0, 45), (90, 0, 45), (25, -130, 78), (-20, 85, 42),]
+list_of_canTravel = [("2", "0 2", "0 1"), ("1 2", "0 2", "0 1"), ("1 2", "0 2", "1 2"), ("1", "0", "0"),]
+list_of_origin = [0,0,0,0,]
+list_of_destination = [1,1,0,2]
+expected_output = [10612.237799994255, 6283.185307179585, 0.0, 5306.118899997127,]
+
+obj = cloud.CloudTravel()
+def test_shortestTrip():
+    for index in range(len(list_of_latitude) - 1):
+        assert obj.shortestTrip(list_of_latitude[index], lsit_of_longitude[index], list_of_canTravel[index],list_of_origin[index], list_of_destination[index]) == expected_output[index]
+
+
+""" 
+def shortestTrip(self, latitude, longitude, canTravel, origin, destination):
+
+print(obj.shortestTrip((0, 0, 70), (90, 0, 45), ("2", "0 2", "0 1"), 0, 1))
+    print(obj.shortestTrip((0, 0, 70), (90, 0, 45), ("1 2", "0 2", "0 1"), 0, 1))
+    print(obj.shortestTrip((0, 30, 60), (25, -130, 78), ("1 2", "0 2", "1 2"), 0, 0))
+    print(obj.shortestTrip((0, 20, 55), (-20, 85, 42), ("1", "0", "0"), 0, 2))
+ """
